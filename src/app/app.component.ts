a45ac0294e9a9d59914fd5450ebaf8863330f121
arbaztalk2me <geekyarbaz@gmail.com>
2024-01-13 15:25:26 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup,NgForm } from '@angular/forms';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'templateDrivenForm';
+  defaultCountry:string="India"
+  formData:any;
+
+  genderData=[
+    {
+      for:"male",
+      id:"male",
+      label:"Male",
+      value:"male"
+    },
+    {
+      for:"female",
+      id:"female",
+      label:"Female",
+      value:"female"
+    },
+    {
+      for:"others",
+      id:"others",
+      label:"Others",
+      value:"others"
+    }
+  ]
+
+  showContent:boolean=false;
+  
+@ViewChild('templateRef') formRef:NgForm
+  
+
+  handleSubmit(){
+    console.log(this.formRef)
+    this.formData=this.formRef.value;
+    this.showContent=this.formRef?.valid;
+    console.log(this.formData)
+    this.formRef.reset();
+  }
+
+
 }
